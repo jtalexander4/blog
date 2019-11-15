ba3c52037e6545df0d1c7be5bf9fbac1af508fb1
John Alexander <john.alexander@xyleminc.com>
2019-11-15 09:54:23 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`article`, `user`],
+        // Possibility to login with a strapi user, when content types are not publically available (Optional).
+        // loginData: {
+        //   identifier: "",
+        //   password: "",
+        // }
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
